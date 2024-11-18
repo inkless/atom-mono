b08f8ddff35e6fbc69775dd08d399c699f8b430f
Guangda Zhang <zhangxiaoyu9350@gmail.com>
2024-11-18 13:23:59 -0800

    update version

--- apps/mobile/app/(protected)/(tabs)/envelope/home.tsx
+++ apps/mobile/app/(protected)/(tabs)/envelope/home.tsx
@@ -13,6 +13,10 @@ export default function EnvelopeHome() {
 
   useEffect(() => {
     if (Platform.OS !== 'web') {
+      // Note, this is not very elegant, it kinda works, we may consider build a
+      // custom native module instead to set cookies
+      // Or maybe we should send Pull Request to react-native-webview to add
+      // this functionality
       CookieManager.set(getBaseUrl(), {
         name: 'session',
         value: session || '',
@@ -36,6 +40,9 @@ export default function EnvelopeHome() {
         source={{
           uri,
         }}
+        // This is not super reliable in Android
+        // https://github.com/react-native-webview/react-native-webview/issues/1609
+        // injectedJavaScript is reliable but it's always after page load.
         // injectedJavaScriptBeforeContentLoaded={`document.cookie="session=${session}"`}
         sharedCookiesEnabled={true}
       />
--- apps/mobile/app/(protected)/(tabs)/index.tsx
+++ apps/mobile/app/(protected)/(tabs)/index.tsx
@@ -4,7 +4,7 @@ import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
-import { greeting } from '@rn/constants';
+import { greeting } from '@atom/constants';
 import { useSession } from '@/contexts/Auth';
 
 export default function HomeScreen() {
