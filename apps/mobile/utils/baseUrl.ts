320d26c28280b711a466197051f5f5c66f829ba6
Guangda Zhang <zhangxiaoyu9350@gmail.com>
2024-11-18 21:50:11 -0800

    update

--- apps/mobile/utils/baseUrl.ts
+++ apps/mobile/utils/baseUrl.ts
@@ -12,16 +12,13 @@ export const getBaseUrl = () => {
    * Gets the IP address of your host-machine. If it cannot automatically find it,
    * you'll have to manually set it. NOTE: Port 3000 should work for most but confirm
    * you don't have anything else running on it, or you'd have to change it.
-   *
-   * **NOTE**: This is only for development. In production, you'll want to set the
-   * baseUrl to your production API URL.
    */
   const debuggerHost = Constants.expoConfig?.hostUri;
   const localhost = debuggerHost?.split(':')[0];
 
   if (!localhost) {
     throw new Error(
-      'Failed to get localhost. Please point to your production server.',
+      'Failed to get localhost. Please use development client and run in local machine.',
     );
   }
   return `http://${localhost}:4000`;
