2e83c10e9ee5ac4fe4b592b92501677489543b55
Guangda Zhang <zhangxiaoyu9350@gmail.com>
2024-12-17 11:44:48 -0800

    support web

--- apps/mobile/utils/baseUrl.ts
+++ apps/mobile/utils/baseUrl.ts
@@ -1,4 +1,5 @@
 import Constants from 'expo-constants';
+import { Platform } from 'react-native';
 
 /**
  * Extend this function when going to production by
@@ -8,6 +9,11 @@ export const getBaseUrl = () => {
   if (process.env.EXPO_PUBLIC_WEB_URL) {
     return process.env.EXPO_PUBLIC_WEB_URL;
   }
+
+  if (Platform.OS === 'web') {
+    return `http://${location.hostname}:4000`;
+  }
+
   /**
    * Gets the IP address of your host-machine. If it cannot automatically find it,
    * you'll have to manually set it. NOTE: Port 3000 should work for most but confirm
