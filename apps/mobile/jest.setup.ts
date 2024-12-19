ab75b5c54e734898267712692559582bdafbd4d0
Guangda Zhang <zhangxiaoyu9350@gmail.com>
2024-12-18 22:07:20 -0800

    test cleanup

--- apps/mobile/jest.setup.ts
+++ apps/mobile/jest.setup.ts
@@ -1,7 +1,13 @@
 import '@testing-library/react-native/extend-expect';
 import { server } from '@atom/mock-server/server';
+import { getApiUrl } from '@/utils/baseUrl';
+
+jest.mock('@/utils/baseUrl');
 
 beforeAll(() => {
+  // in jest test, there's no host uri, we just mock it
+  (getApiUrl as jest.Mock).mockReturnValue('http://localhost');
+
   // Start the interception.
   server.listen();
 });
